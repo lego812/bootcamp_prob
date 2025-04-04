882c395b9b65a86344b7b067d0db02d0469740f9
lego812 <suwontaran@hs.ac.kr>
2025-04-04 22:13:43 +0900

    -07vue 컴포넌트 심화 기본문제 완료

---
+++ vue_prob/07_vue/slot-test/src/components/CheckBox2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-changed', { id, checked: $event.target.checked })"
+    />
+    <slot>Item</slot>
+  </div>
+</template>
+
+<script>
+export default { name: 'checkBox2', props: ['id', 'checked'] }
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue_prob/07_vue/slot-test/src/components/SlotTest.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3>당신이경험한프론트엔드기술은?(첫번째:Slot사용(X))</h3>
+    <CheckBox2
+      v-for="item in items"
+      :key="item.id"
+      :checked="item.checked"
+      :label="item.label"
+      @check-changed="CheckBoxChanged"
+    >
+      <span v-if="item.checked === true" style="color: blue; text-decoration: underline">
+        <i>{{ item.label }}</i>
+      </span>
+      <span v-else style="color: gray">{{ item.label }}</span>
+    </CheckBox2>
+  </div>
+</template>
+
+<script>
+import CheckBox2 from './CheckBox2.vue'
+export default {
+  name: 'NoSlotTest',
+  components: { CheckBox2 },
+  data() {
+    S
+    return {
+      items: [
+        { id: 'V', checked: true, label: 'Vue' },
+        { id: 'A', checked: false, label: 'Angular' },
+        { id: 'R', checked: false, label: 'React' },
+        { id: 'S', checked: false, label: 'Svelte' },
+      ],
+    }
+  },
+  methods: {
+    CheckBoxChanged(e) {
+      let item = this.items.find((item) => item.id === e.id)
+      item.checked = e.checked
+    },
+  },
+}
+</script>
