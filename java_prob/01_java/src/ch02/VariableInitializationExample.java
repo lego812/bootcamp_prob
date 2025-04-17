c3a83b03ac85c31594753ae9bca55ec4fb276e2a
lego812 <suwontaran@hs.ac.kr>
2025-04-17 14:33:46 +0900

    자바 1장 추가 완료

---
+++ java_prob/01_java/src/ch02/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+//package ch02;
+//
+//public class VariableInitializationExample {
+//    public static void main(String[] args) {
+//        int value; //
+//        int result = value + 10;
+//        System.out.println(result);
+//    }
+//
+//}
+
+//value에 값이 할당되지 않은 상태로 쓰였다.
