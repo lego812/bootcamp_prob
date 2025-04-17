6d408b9fb29f8141a78877ccad25187b81f58735
lego812 <suwontaran@hs.ac.kr>
2025-04-17 17:55:16 +0900

    2장 기본일부...(겁나많네)

---
+++ java_prob/02_java/src/ch04/sec06/DoWhileExample.java
@@ -0,0 +1,18 @@
+package ch04.sec06;
+
+import java.util.Scanner;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("메시지를 입력하세요.");
+        System.out.println("프로그램을 종료하려면 q를 입력하세요.");
+
+        do {
+            System.out.println(">");
+            String str=scanner.nextLine();
+            System.out.println(str);
+        }while(!str.equls("q"));
+
+    }
+}
