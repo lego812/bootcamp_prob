63713f44c3c97cb98138b5b6ef02417374b39656
lego812 <suwontaran@hs.ac.kr>
2025-04-22 17:48:13 +0900

    4장 기본 문제 완료

--- java_prob/04_java/src/ch07/sec03/exam02/SmartPhoneExample.java
+++ java_prob/04_java/src/ch07/sec03/exam02/SmartPhoneExample.java
@@ -3,5 +3,7 @@ package ch07.sec03.exam02;
 public class SmartPhoneExample {
     public static void main(String[] args) {
         Smartphone smartPhone=new Smartphone("자가용", "빨강");
+        System.out.println(smartPhone.model);
+        System.out.println(smartPhone.color);
     }
 }
--- java_prob/04_java/src/ch07/sec03/exam02/Smartphone.java
+++ java_prob/04_java/src/ch07/sec03/exam02/Smartphone.java
@@ -2,6 +2,7 @@ package ch07.sec03.exam02;
 
 public class Smartphone extends Phone{
     public Smartphone(String model, String color) {
+        super(model, color);
         this.model=model;
         this.color=color;
     }
