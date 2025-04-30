d92b8558009d468fa144f123aa594382369e8a41
lego812 <suwontaran@hs.ac.kr>
2025-04-30 17:15:16 +0900

    자바 9장 심화 완료

--- back/java_prob/09_java_deep/src/ch15/sec02/exam01/Board.java
+++ back/java_prob/09_java_deep/src/ch15/sec02/exam01/Board.java
@@ -1,40 +1,23 @@
 package ch15.sec02.exam01;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Board {
     private String subject;
     private String content;
     private String writer;
 
     //기본 데이터 클래스 작성
-    public Board(String subject, String content, String writer) {
-        this.subject = subject;
-        this.content = content;
-        this.writer = writer;
-    }
-
-    //getter, setter
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
+//    public Board(String subject, String content, String writer) {
+//        this.subject = subject;
+//        this.content = content;
+//        this.writer = writer;
+//    }
 }
+
