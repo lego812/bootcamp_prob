369160ff3d0206a9f9a5a54b69f0a4143866275c
lego812 <suwontaran@hs.ac.kr>
2025-04-30 16:54:44 +0900

    자바 10장 완료

---
+++ back/java_prob/10_java/src/ch15/sec06/exam01/Coin.java
@@ -0,0 +1,14 @@
+package ch15.sec06.exam01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@Getter
+public class Coin {
+    private int value;
+}
