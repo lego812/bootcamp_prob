b6e7ac64e6d9ee16a15a94b561ac3f3863c3262b
lego812 <suwontaran@hs.ac.kr>
2025-05-07 09:50:22 +0900

    Revert "실습체크리스트 업데이트"
    This reverts commit 1878012e754dfefced05f508bafc900593e3eee8.

--- back/java_prob/06_java/src/ch09/sec02/exam01/AExample.java
+++
@@ -1,9 +0,0 @@
-package ch09.sec02.exam01;
-
-public class AExample {
-    public static void main(String[] args) {
-        A a = new A();
-
-        A.B b=a.new B();
-    }
-}
--- back/java_prob/06_java/src/ch09/sec03/exam01/AExample.java
+++
@@ -1,7 +0,0 @@
-package ch09.sec03.exam01;
-
-public class AExample {
-    public static void main(String[] args) {
-        A.B b = new A.B();
-    }
-}
