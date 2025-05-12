2a1aef15ac166f7e5909f92dc3fce0ce36c3451e
lego812 <suwontaran@hs.ac.kr>
2025-05-12 15:14:50 +0900

    자바 13장 심화 완료

---
+++ back/java_prob/13_java_deep/src/ch18/sec10/ObjectInputStreamExample.java
@@ -0,0 +1,22 @@
+package ch18.sec10;
+
+import java.io.*;
+import java.util.*;
+
+public class ObjectInputStreamExample {
+    public static void main(String[] args) throws Exception{
+        FileInputStream fis = new FileInputStream("C:/Temp/object.dat");
+        ObjectInputStream ois = new ObjectInputStream
+                (fis);
+//파일을 읽고 역질렬화해서 객체로 복원
+        Member m2 = (Member) ois.readObject();
+        Product p2 = (Product) ois.readObject();
+        int[] arr2 = (int[]) ois.readObject();
+        ois.close(); fis.close();
+//복원된 객체 내용 확인
+        System.out.println(m2);
+        System.out.println(p2);
+        System.out.println(Arrays.toString(arr2));
+
+    }
+}
