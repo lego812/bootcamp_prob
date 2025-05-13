6e6b1f0d58801d095c1d03f8d41d9bee7fce6846
lego812 <suwontaran@hs.ac.kr>
2025-05-13 17:39:53 +0900

    db 1장,2장 기본 심화 완료

---
+++ back/db_prob/02_db/02_05번문제.sql
@@ -0,0 +1,7 @@
+select * from producttbl;
+
+select memberName, memberAddress from membertbl;
+
+-- delete from membertbl where memberName='박다빈';
+
+select * from membertbl where memberName='지운이';
---
+++ back/db_prob/02_db_deep/02_3번문제.sql
@@ -0,0 +1,3 @@
+SELECT * FROM tbl_board;
+select no, title, writer from tbl_board;
+select * from tbl_board where writer='박다빈';
